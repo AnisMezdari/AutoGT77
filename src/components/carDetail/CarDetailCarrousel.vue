668688114127a6d25a4ed41126746a7fb4f1d59e
Anis Mezdari <mezdari.anis@gmail.com>
2019-01-22 14:18:20 +0100

    fix bug / improvment

---
+++ src/components/carDetail/CarDetailCarrousel.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="containerCarDetailCarrousel">
+    <div class="row carDetailTitleCarrousel">
+      <h4>{{title}}</h4>
+    </div>
+    <div class="col-10 carrouselContainer">
+      <b-carousel id="carousel1"
+                   style="text-shadow: 1px 1px 2px #333;"
+                   controls
+                   indicators
+                   :interval="5000"
+                   img-width="1024"
+                   img-height="480"
+
+
+       >
+         <!-- Text slides with image -->
+         <b-carousel-slide>
+           <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             :src="require(`./assets/${carImage}`)"
+             :alt = "title" />
+         </b-carousel-slide>
+
+         <!-- Slides with custom text -->
+         <b-carousel-slide>
+           <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             :src="require(`./assets/${carImageDeux}`)"
+             :alt = "title" />
+         </b-carousel-slide>
+
+         <!-- Slides with image only -->
+         <b-carousel-slide>
+           <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             :src="require(`./assets/${carImageTrois}`)"
+             :alt = "title" />
+         </b-carousel-slide>
+
+         <b-carousel-slide>
+           <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             :src="require(`./assets/${carImageQuatre}`)"
+             :alt = "title" />
+         </b-carousel-slide>
+       </b-carousel>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+    export default {
+      name: 'CarDetailCarrousel',
+      props:["title","carImage","carImageDeux","carImageTrois","carImageQuatre"],
+      components: {
+     },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .carDetailTitleCarrousel{
+    font-family: Impact;
+    color : #22313F;
+    margin-bottom:  15px;
+  }
+
+  .carrouselContainer{
+  }
+</style>
---
+++ src/components/carDetail/CarDetailDescription.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class=" row CarDetailDescriptionContainer">
+    <div class="col-3 globalDescription">
+      <ul>
+        <li> <b>Price</b> : 4500€</li>
+        <li> <b> Kilometer</b> : 125 000km</li>
+        <li> <b>Years</b> : 2015</li>
+      </ul>
+    </div>
+    <p class ="col-7">
+      Verum ad istam omnem orationem brevis est defensio.
+      Nam quoad aetas M. Caeli dare potuit isti suspicioni locum,
+      fuit primum ipsius pudore, deinde etiam patris diligentia disciplinaque munita.
+      Qui ut huic virilem togam deditšnihil dicam hoc loco de me; tantum sit,
+      quantum vos existimatis; hoc dicam, hunc a patre continuo ad me esse deductum;
+      nemo hunc M. Caelium in illo aetatis flore vidit nisi aut cum patre aut mecum
+      aut in M. Crassi castissima domo, cum artibus honestissimis erudiretur.
+    </p>
+  </div>
+
+
+</template>
+
+<script>
+    export default {
+      name: 'CarDetailDescription',
+      props:[],
+      components: {
+     },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .CarDetailDescriptionContainer{
+
+    font-size: 15px;
+    font-family: Cambria;
+    margin-top: 15px;
+  }
+  .globalDescription{
+    margin-right: -50px;
+  }
+
+</style>
